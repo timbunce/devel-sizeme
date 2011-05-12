76ab8e98000aa877da1e157080c1fb146732a93a
Nicholas Clark <nick@ccl4.org>
2011-05-12 17:34:34 +0100

    Bump $VERSION to 0.77

--- lib/Devel/Size.pm
+++ lib/Devel/Size.pm
@@ -14,7 +14,7 @@ require XSLoader;
 # This allows declaration   use Devel::Size ':all';
 %EXPORT_TAGS = ( 'all' => \@EXPORT_OK );
 
-$VERSION = '0.76_50';
+$VERSION = '0.77';
 
 XSLoader::load( __PACKAGE__);
 
