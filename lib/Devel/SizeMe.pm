0520573845fbde83fb99d51310d5ae578ba0e6db
Tim Bunce <Tim.Bunce@pobox.com>
2012-10-03 14:51:40 +0100

    bump version

--- lib/Devel/SizeMe.pm
+++ lib/Devel/SizeMe.pm
@@ -27,7 +27,7 @@ require 5.005;
 require Exporter;
 require XSLoader;
 
-$VERSION = '0.020_080';
+$VERSION = '0.020_081';
 @ISA = qw(Exporter);
 
 @EXPORT_OK = qw(size total_size perl_size heap_size);
