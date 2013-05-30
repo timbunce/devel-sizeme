fb00a69b19e43bebb99d2c007955d8cd54953e9a
Tim Bunce <Tim.Bunce@pobox.com>
2013-05-30 16:48:47 -0700

    A bunch of treemap UI cleanup thanks to Avi Mozes

--- bin/sizeme_graph.pl
+++ bin/sizeme_graph.pl
@@ -364,7 +364,7 @@ Welcome to the Mojolicious real-time web framework!
                 <a id="goto_parent" href="#" class="theme button white">Go to Parent</a>
                 <form name=params id="sizeme_form">
                 <label for="logarea">Log scale
-                <input type=checkbox id="logarea" name="logarea">
+                <input type=checkbox id="sizeme_logarea" name="logarea">
                 </form>
                 </p>
             </div>
