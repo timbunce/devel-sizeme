371ab00a5f3337e04a355409b1ddfae57346700a
Tim Bunce <Tim.Bunce@pobox.com>
2013-05-31 16:12:58 -0500

    prep for 0.12 release

--- lib/Devel/SizeMe.pm
+++ lib/Devel/SizeMe.pm
@@ -27,7 +27,7 @@ require 5.008;
 require Exporter;
 require XSLoader;
 
-$VERSION = '0.11';
+$VERSION = '0.12';  # also update t/00-load.t
 @ISA = qw(Exporter);
 
 @EXPORT_OK = qw(size total_size perl_size heap_size);
